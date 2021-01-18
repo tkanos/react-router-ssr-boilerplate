1f84189f8d4e945309bf1734c2296be3b4a43435
f_dutratineesilva <felipe.dutratineesilva@mindgeek.com>
2021-01-18 15:51:57 -0500

    remove bootstrap

--- src/isomorphic/pages/HomePage/HomePage.js
+++ src/isomorphic/pages/HomePage/HomePage.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import  Button  from 'react-bootstrap/Button';
 
 const HomePage = () => {
     return (
     <div>
         <div> I'm the Home component</div>
-       <Button onClick={ () => console.log('Hello') }>Press Me</Button>
+       <button onClick={ () => console.log('Hello') }>Press Me</button>
     </div>)
 }
 
--- src/server/templates/mainPage.js
+++ src/server/templates/mainPage.js
@@ -24,12 +24,6 @@ export default (path, store, context) => {
     <head>
      ${helmet.title.toString()}
      ${helmet.meta.toString()}
-      <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-      crossorigin="anonymous"
-    />
     </head>
     <body>
       <div id="root">${content}</div>
