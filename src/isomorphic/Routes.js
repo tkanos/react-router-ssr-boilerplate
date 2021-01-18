bebc0cdbfc0413b6ca4db517566c78767df623c7
f_dutratineesilva <felipe.dutratineesilva@mindgeek.com>
2021-01-18 11:01:49 -0500

    adding header

--- src/isomorphic/Routes.js
+++ src/isomorphic/Routes.js
@@ -1,26 +1,22 @@
 import React from 'react'
-//import { Route } from 'react-router-dom'
+import App from './App'
 import HomePage from './pages/HomePage/HomePage'
 import UserListPage, { initData } from './pages/UsersListPage/UserListPage.js'
 
-/*
-export default () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/users" component={UserList} />
-        </div>
-    )
-
-}*/
 export default [
     {
-        ...HomePage,
-        path: '/',
-        exact: true
-    },
-    {
-        ...UserListPage,
-        path: '/users',
+        ...App,
+        routes: [
+            {
+                ...HomePage,
+                path: '/',
+                exact: true
+            },
+            {
+                ...UserListPage,
+                path: '/users',
+            }
+        ]
     }
-  ]
+]
+
