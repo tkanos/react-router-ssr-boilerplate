1825090f565a9716286aece4eee0d2fad7ddb8df
f_dutratineesilva <felipe.dutratineesilva@mindgeek.com>
2021-01-19 10:29:33 -0500

    use hooks instead of connect

--- src/isomorphic/pages/UsersListPage/UserListPage.js
+++ src/isomorphic/pages/UsersListPage/UserListPage.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component, useEffect } from 'react'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import { fetchUsers } from '../../actions'
 import { Helmet } from 'react-helmet'
 
-class UserListPage extends Component {
-    componentDidMount() {
-        this.props.fetchUsers();
-    }
+const UserListPage = () =>  {
+    const dispatch = useDispatch();
+    const users = useSelector(state => state.users);
 
-    renderUsers() {
-        return this.props.users.map(user => {
+    useEffect(() => {
+        dispatch(fetchUsers())
+      })
+
+    const renderUsers = () => {
+        return users.map(user => {
             return <li key={user.id}>{user.name}</li>
         })
     }
-
-    head() {
+    const head = () => {
         return (
         <Helmet>
-            <title>{`${this.props.users.length} Users Loaded`}</title>
+            <title>{`${users.length} Users Loaded`}</title>
             <meta property="og.title" content="Users List" />  
         </Helmet>
         )
     }
-
-    render() {
-        return (
-        <div>
-            {this.head()}
-            User List:
-            <ul>{this.renderUsers()}</ul>
-        </div>)
-    }
+   
+    return (
+    <div>
+        {head()}
+        User List:
+        <ul>{renderUsers()}</ul>
+    </div>)
+    
 }
 
-function mapStateToProps(state) {
-    return { users: state.users }
-}
-
-function initData(store) {
+function initData() {
     return store.dispatch(fetchUsers())
 }
 
 export default {
     initData,
-    component: connect(mapStateToProps, { fetchUsers })(UserListPage)
+    component: UserListPage
 }
